019592a0f906e9c20d01af64c353a531ff8e7c14
Finn Shepherd <shepherf@flip1.engr.oregonstate.edu>
2023-11-08 17:06:26 -0800

    added joins in dml file

--- tosDML.sql
+++ tosDML.sql
@@ -1,35 +1,51 @@
 -- Products
 -- CREATE
-INSERT INTO Products(productName, description, unitPrice, quantityInStock, supplierID, categoryID)
+INSERT INTO Products (productName, description, unitPrice, quantityInStock, supplierID, categoryID)
 VALUES (:productName, :description, :unitPrice, :quantityInStock, :supplierID, :categoryID);
 
 -- SELECT
-SELECT * FROM Products WHERE productID = :productID;
+SELECT
+    P.productID,
+    P.productName,
+    P.description,
+    P.unitPrice,
+    P.quantityInStock,
+    S.supplierName,
+    PC.category
+FROM Products P
+INNER JOIN Suppliers S ON P.supplierID = S.supplierID
+INNER JOIN ProductCategories PC ON P.categoryID = PC.categoryID
+WHERE P.productID = :productID;
 
--- Select all table data from Products
-SELECT * FROM Products;
+-- SELECT ALL
+SELECT
+    P.productID,
+    P.productName,
+    P.description,
+    P.unitPrice,
+    P.quantityInStock,
+    S.supplierName,
+    PC.category
+FROM Products P
+INNER JOIN Suppliers S ON P.supplierID = S.supplierID
+INNER JOIN ProductCategories PC ON P.categoryID = PC.categoryID;
 
 -- DELETE
--- Delete a product and related entries in ProductSales(on delete CASCADE in ProductSales)
-DELETE FROM Products
-WHERE productID = :productID;
+-- Delete a product and related entries in ProductSales (on delete CASCADE in ProductSales)
+DELETE P, PS
+FROM Products P
+INNER JOIN ProductSales PS ON P.productID = PS.productID
+WHERE P.productID = :productID;
 
 -- Customers
 -- CREATE
-INSERT INTO Customers(firstName, lastName, email, phone, address)
+INSERT INTO Customers (firstName, lastName, email, phone, address)
 VALUES (:firstName, :lastName, :email, :phone, :address);
 
--- SELECT including search feature
-SELECT * FROM Customers
-WHERE
-    (:searchKeyword IS NULL OR
-    (LOWER(firstName) LIKE CONCAT('%', LOWER(:searchKeyword), '%') OR
-    LOWER(lastName) LIKE CONCAT('%', LOWER(:searchKeyword), '%') OR
-    LOWER(email) LIKE CONCAT('%', LOWER(:searchKeyword), '%') OR
-    LOWER(phone) LIKE CONCAT('%', LOWER(:searchKeyword), '%') OR
-    LOWER(address) LIKE CONCAT('%', LOWER(:searchKeyword), '%')));
+-- SELECT
+SELECT * FROM Customers WHERE customerID = :customerID;
 
--- Select all table data from Customers
+-- SELECT ALL
 SELECT * FROM Customers;
 
 -- UPDATE
@@ -40,52 +56,69 @@ WHERE customerID = :customerID;
 
 -- SalesOrders
 -- CREATE
-INSERT INTO SalesOrders(orderDate, customerID)
+INSERT INTO SalesOrders (orderDate, customerID)
 VALUES (:orderDate, :customerID);
 
 -- SELECT
 SELECT * FROM SalesOrders WHERE orderID = :orderID;
 
--- Select all table data from SalesOrders
+-- SELECT ALL
 SELECT * FROM SalesOrders;
 
 -- No DELETE or UPDATE operations provided for SalesOrders.
 
 -- Suppliers
 -- CREATE
-INSERT INTO Suppliers(supplierName, contactName, email, phone, address)
+INSERT INTO Suppliers (supplierName, contactName, email, phone, address)
 VALUES (:supplierName, :contactName, :email, :phone, :address);
 
 -- SELECT
 SELECT * FROM Suppliers WHERE supplierID = :supplierID;
 
--- Select all table data from Suppliers
+-- SELECT ALL
 SELECT * FROM Suppliers;
 
 -- No DELETE or UPDATE operations provided for Suppliers.
 
 -- ProductSales (Many-to-Many relationship)
 -- CREATE
-INSERT INTO ProductSales(productID, orderID, quantitySold, salePrice)
+INSERT INTO ProductSales (productID, orderID, quantitySold, salePrice)
 VALUES (:productID, :orderID, :quantitySold, :salePrice);
 
 -- SELECT
-SELECT * FROM ProductSales WHERE productSaleID = :productSaleID;
+SELECT
+    PS.productSaleID,
+    PS.quantitySold,
+    PS.salePrice,
+    P.productName,
+    SO.orderDate
+FROM ProductSales PS
+INNER JOIN Products P ON PS.productID = P.productID
+INNER JOIN SalesOrders SO ON PS.orderID = SO.orderID
+WHERE PS.productSaleID = :productSaleID;
 
--- Select all table data from ProductSales
-SELECT * FROM ProductSales;
+-- SELECT ALL
+SELECT
+    PS.productSaleID,
+    PS.quantitySold,
+    PS.salePrice,
+    P.productName,
+    SO.orderDate
+FROM ProductSales PS
+INNER JOIN Products P ON PS.productID = P.productID
+INNER JOIN SalesOrders SO ON PS.orderID = SO.orderID;
 
 -- No DELETE or UPDATE operations provided for ProductSales.
 
 -- ProductCategories
 -- CREATE
-INSERT INTO ProductCategories(category)
+INSERT INTO ProductCategories (category)
 VALUES (:category);
 
 -- SELECT
 SELECT * FROM ProductCategories WHERE categoryID = :categoryID;
 
--- Select all table data from ProductCategories
+-- SELECT ALL
 SELECT * FROM ProductCategories;
 
 -- No DELETE or UPDATE operations provided for ProductCategories.
