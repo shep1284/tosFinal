428483f0e064e94654bdfcee17aa25bf2da7fa10
Tyler J Sphar <77037935+tylerjsphar@users.noreply.github.com>
2023-11-10 15:37:42 -0800

    Setup handlebars to display data from db

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 4124;
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
  
 // Serve static files (e.g., HTML, CSS, JavaScript)
-app.use(express.static(path.join(__dirname, 'public')));
- 
+//app.use(express.static(path.join(__dirname, 'public')));
+// Commented out while getting handlebars working
+
 // Handle the root route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+app.get('/', function(req, res)
+    {
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
+
+
  
 // Start the server
 app.listen(port, () => {
