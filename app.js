36336bbae82f7b21ea7db8544510bc2c1886746e
Tyler J Sphar <77037935+tylerjsphar@users.noreply.github.com>
2023-11-14 13:58:06 -0800

    Implemented routing and data display
    Implemented routing and data display for each table

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = 4124;
 
+
 // Database
 var db = require('./database/db-connector');
 
@@ -12,19 +13,76 @@ app.set('view engine', '.hbs');                 // Tell express to use the handl
 
  
 // Serve static files (e.g., HTML, CSS, JavaScript)
-//app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 // Commented out while getting handlebars working
 
 // Handle the root route
 app.get('/', function(req, res)
     {  
+
+        res.render('index');                  // Render the index.hbs file, and also send the renderer                                                     // an object where 'data' is equal to the 'rows' we
+    });     
+                                                        // received back from the query 
+app.get('/products', function(req, res)
+    {  
         let query1 = "SELECT * FROM Products;";               // Define our query
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+            res.render('products', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });     
+                                                        // received back from the query 
+app.get('/categories', function(req, res)
+    {  
+        let query1 = "SELECT * FROM ProductCategories;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('categories', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });            
+                                                 // received back from the query 
+app.get('/suppliers', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Suppliers;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('suppliers', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });          
+                                                   // received back from the query 
+app.get('/customers', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Customers;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('customers', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });       
+                                                      // received back from the query 
+app.get('/salesorders', function(req, res)
+    {  
+        let query1 = "SELECT * FROM SalesOrders;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('salesorders', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
     });                                                         // received back from the query 
+
+app.get('/productsales', function(req, res)
+    {  
+        let query1 = "SELECT * FROM ProductSales;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('productsales', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query 
+
 
 
  
